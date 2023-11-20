b2f3833dbcd5298a3b68c12cc2d0e63c47c1b4b5
Salmanul Faris M P <salmanul@yougotagift.com>
2023-11-20 17:13:32 +0530

    stashing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Sample from "@/features/sample/Sample";
+import CorporateServiceDetails from "@/features/CorporateServiceDetails/CorporateServiceDetails";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Sample />
+      <CorporateServiceDetails />
     </main>
   );
 }
--- src/features/CorporateServiceDetails/CorperateServiceContent.tsx
+++ src/features/CorporateServiceDetails/CorperateServiceContent.tsx
@@ -4,7 +4,16 @@ import styles from "./CorperateServiceDetails.module.scss";
 const CorperateServiceContent = () => {
   return (
     <div className={styles["corperate-service-content"]}>
-      CorperateServiceContent
+      Experience the ultimate shopping with Nakheel mall Gift Cards! Our Gift
+      Cards are your passport to the diverse world of retail within our mall.
+      When you purchase one of our Gift Cards, you're not just giving a present;
+      you're gifting an unforgettable experience. Simply load your Gift Card
+      with your preferred amount, and you're ready to explore our myriad of
+      stores. Present your card at checkout, and watch your purchase expenses
+      subtract from the card's balance. For added convenience, check your card's
+      balance online as needed. Treat yourself or your loved ones to an
+      exceptional Nakheel mall adventure with our Gift Cards—the perfect way to
+      celebrate any occasion.
     </div>
   );
 };
